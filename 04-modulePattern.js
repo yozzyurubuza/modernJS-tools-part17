ddf2b608a3817a47c9c64a6fc09ce3e13a9e6003
yozzyurubuza <matt_ebarle15@yahoo.com>
2022-02-22 11:21:17 +0800

    Added codes about The Module Pattern

---
+++ 04-modulePattern.js
@@ -0,0 +1,34 @@
+//The Module Pattern
+
+//Encapsulate global variables
+const ShoppingCart2 = (function () {
+  const cart = [];
+  const shippingCost = 10;
+  const totalPrice = 237;
+  const totalQuantity = 23;
+
+  const addToCart = function (product, quantity) {
+    cart.push(product, quantity);
+    console.log(
+      `${quantity} ${product} added to cart (shipping cost is ${shippingCost})`
+    );
+  };
+
+  const orderStock = function (product, quantity) {
+    console.log(`${quantity} ${product} ordered from supplier`);
+  };
+
+  //Return variables or methods that you want to expose
+  return {
+    addToCart,
+    cart,
+    totalPrice,
+    totalQuantity,
+  };
+})();
+
+//ShoppingCart2 is not accessible in the browser's console
+ShoppingCart2.addToCart('apple', 4);
+ShoppingCart2.addToCart('pizza', 2);
+console.log(ShoppingCart2);
+console.log(ShoppingCart2.shippingCost); //not accessible
