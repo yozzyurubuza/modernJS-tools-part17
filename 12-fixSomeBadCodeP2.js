19149fed99bd1b789bef1e4b32b45cde83edb418
yozzyurubuza <matt_ebarle15@yahoo.com>
2022-02-28 22:46:31 +0800

    Added codes about Let's Fix Some Bad Code: Part 2

---
+++ 12-fixSomeBadCodeP2.js
@@ -0,0 +1,113 @@
+//Let's Fix Some Bad Code Part 2
+'use strict';
+
+//Object.freeze only freezes the first level of the object
+const budget = Object.freeze([
+  { value: 250, description: 'Sold old TV 📺', user: 'jonas' },
+  { value: -45, description: 'Groceries 🥑', user: 'jonas' },
+  { value: 3500, description: 'Monthly salary 👩‍💻', user: 'jonas' },
+  { value: 300, description: 'Freelancing 👩‍💻', user: 'jonas' },
+  { value: -1100, description: 'New iPhone 📱', user: 'jonas' },
+  { value: -20, description: 'Candy 🍭', user: 'matilda' },
+  { value: -125, description: 'Toys 🚂', user: 'matilda' },
+  { value: -1800, description: 'New Laptop 💻', user: 'jonas' },
+]);
+
+//Object.freeze - make object immutable
+const spendingLimits = Object.freeze({
+  jonas: 1500,
+  matilda: 100,
+});
+// spendingLimits.jay = 200; // Cannot add, returns error
+
+//Pass-in spendingLimits as limits to avoid outside mutation
+const getLimit = (limits, user) => limits?.[user] ?? 0;
+
+//Pure Function - Does not mutate outside variables anymore
+const addExpense = function (
+  state,
+  limits,
+  value,
+  description,
+  user = 'jonas'
+) {
+  //Avoid data mutation
+  //   user = user.toLowerCase();
+  const cleanUser = user.toLowerCase();
+
+  //Transforming if to Ternary Operator
+  return value <= getLimit(limits, cleanUser)
+    ? [...state, { value: -value, description, user: cleanUser }]
+    : state;
+
+  //This code has side effect, something outside of a function is manipulated.
+  // budget.push({ value: -value, description, user: cleanUser });
+};
+
+const newBudget1 = addExpense(budget, spendingLimits, 10, 'Pizza 🍕');
+const newBudget2 = addExpense(
+  newBudget1,
+  spendingLimits,
+  100,
+  'Going to movies 🍿',
+  'Matilda'
+);
+const newBudget3 = addExpense(newBudget2, spendingLimits, 200, 'Stuff', 'Jay');
+
+//Transform into a Pure Function
+const checkExpenses2 = function (state, limits) {
+  //Transform for of loop and return a new copy of state
+  return state.map(entry => {
+    return entry.value < -getLimit(limits, entry.user)
+      ? { ...entry, flag: 'limit' }
+      : entry;
+  });
+
+  // Old Code
+  // for (const entry of budget)
+  // if (entry.value < -getLimit(limits, entry.user)) entry.flag = 'limit';
+};
+
+//Simplified Version
+const checkExpenses = (state, limits) =>
+  state.map(entry =>
+    entry.value < -getLimit(limits, entry.user)
+      ? { ...entry, flag: 'limit' }
+      : entry
+  );
+
+const finalBudget = checkExpenses(newBudget3, spendingLimits);
+console.log(finalBudget);
+
+//Transform into Pure Function
+const logBigExpenses = function (state, bigLimit) {
+  return state
+    .reduce(
+      (acc, entry) =>
+        (acc +=
+          entry.value <= -bigLimit ? `${entry.description.slice(-2)} / ` : ''),
+      ''
+    )
+    .slice(0, -2);
+
+  //Jonas Schmedtmann Implementation
+
+  //   const bigExpenses = state
+  //     .filter(entry => entry.value <= -bigLimit)
+  //     .map(entry => entry.description.slice(-2))
+  //     .join(' / ');
+
+  //   console.log(bigExpenses);
+
+  //Old Code
+  //   let output = '';
+  //   for (const entry of budget)
+  //     output +=
+  //       entry.value <= -bigLimit ? `${entry.description.slice(-2)} / ` : '';
+
+  //   output = output.slice(0, -2); // Remove last '/ '
+  //   console.log(output);
+};
+
+const getBigExpenses = logBigExpenses(finalBudget, 1000);
+console.log(getBigExpenses); //console.logs are impure (side effects)
